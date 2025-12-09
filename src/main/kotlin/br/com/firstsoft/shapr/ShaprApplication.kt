2f3bb875366e0207e8618d94b9fa63acbad7288c
Danilo Lemes <daniloleemes@hotmail.com>
2025-12-09 12:33:12 +0000

    initial commit

---
+++ src/main/kotlin/br/com/firstsoft/shapr/ShaprApplication.kt
@@ -0,0 +1,11 @@
+package br.com.firstsoft.shapr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShaprApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShaprApplication>(*args)
+}
